d1dde9cd98f1660f05d4ab1e8849b08d782883e7
Juliane <julianesilvac75@gmail.com>
2022-10-25 15:21:33 +0100

    fixation: test exception

--- computer-science/1-introducao-a-python/dia3-testes/fixation/codigo.py
+++ computer-science/1-introducao-a-python/dia3-testes/fixation/codigo.py
@@ -1,3 +1,8 @@
 def is_odd(number):
     'Retorna True se um numero eh impar, senao False.'
     return number % 2 != 0
+
+
+def divide(a_number, other_number):
+    'Retorna a divisao de a_number por other_number.'
+    return a_number / other_number
--- computer-science/1-introducao-a-python/dia3-testes/fixation/test_codigo.py
+++ computer-science/1-introducao-a-python/dia3-testes/fixation/test_codigo.py
@@ -1,4 +1,6 @@
-from codigo import is_odd
+import pytest
+from codigo import is_odd, divide
+
 
 def test_is_odd_when_number_is_odd_returns_true():
     'Para um numero impart a funcao deve retornar o valor True.'
@@ -8,3 +10,8 @@ def test_is_odd_when_number_is_odd_returns_true():
 def test_is_odd_when_number_is_even_returns_false():
     'Para um numero par a funcao deve retornar o valor False.'
     assert is_odd(2) is False
+
+
+def test_divide_when_other_number_is_zero_raises_an_exception():
+    with pytest.raises(ZeroDivisionError, match="division by zero"):
+        divide(2, 0)
