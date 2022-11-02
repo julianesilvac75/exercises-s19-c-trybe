9b557a78bb4fefd8e28fcf9c4da051e961087663
Juliane <julianesilvac75@gmail.com>
2022-11-02 14:02:02 +0000

    fixation: observer exercise

--- computer-science/2-padroes-de-projeto/dia3-padroes-decorator-observer-factory/fixation/observer/cliente.py
+++ computer-science/2-padroes-de-projeto/dia3-padroes-decorator-observer-factory/fixation/observer/cliente.py
@@ -9,8 +9,18 @@ if __name__ == "__main__":
     seguidores_email = ["Claudia", "Marcia"]
 
     meu_perfil = Perfil()
-    NotificadorMensagem(meu_perfil, seguidores_mensagem)
-    NotificadorPushNotification(meu_perfil, seguidores_push_notification)
-    NotificadorEmail(meu_perfil, seguidores_email)
+    notificador_mensagem = NotificadorMensagem(meu_perfil, seguidores_mensagem)
+    notificador_push_notification = NotificadorPushNotification(meu_perfil, seguidores_push_notification)
+    notificador_email = NotificadorEmail(meu_perfil, seguidores_email)
 
     meu_perfil.adicionar_post("Ola universo da Programacao!")
+
+    meu_perfil.remover_sistema_de_notificacao(notificador_email)
+    meu_perfil.adicionar_post("Esse eh o post sem email")
+
+    meu_perfil.adicionar_sistema_de_notificacao(notificador_email)
+    meu_perfil.adicionar_post("Esse eh o post com todos")
+
+    meu_perfil.remover_sistema_de_notificacao(notificador_push_notification)
+    meu_perfil.adicionar_post("Esse eh o post sem push notification")
+
