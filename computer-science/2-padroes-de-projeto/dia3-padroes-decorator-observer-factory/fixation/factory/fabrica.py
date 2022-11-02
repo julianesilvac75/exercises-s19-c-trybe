083c5014d98c18a77e7c8d793cf5e69d51401b8d
Juliane <julianesilvac75@gmail.com>
2022-11-02 14:25:36 +0000

    factory: creating combo factories

--- computer-science/2-padroes-de-projeto/dia3-padroes-decorator-observer-factory/fixation/factory/fabrica.py
+++ computer-science/2-padroes-de-projeto/dia3-padroes-decorator-observer-factory/fixation/factory/fabrica.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+from item_cardapio import ItemFritas, ItemHamburger, ItemRefrigerante, ItemSorvete
+
 
 class Combo(ABC):
     def __init__(self):
@@ -7,7 +9,7 @@ class Combo(ABC):
         self.criar_combo()
 
     @abstractmethod
-    def criar_combo():
+    def criar_combo(self):
         pass
 
     def exibe_itens(self):
@@ -15,3 +17,25 @@ class Combo(ABC):
 
     def adicionar_itens(self, item):
         self.itens.append(item)
+
+
+class ComboTudo(Combo):
+    def criar_combo(self):
+        self.adicionar_itens(ItemHamburger())
+        self.adicionar_itens(ItemSorvete())
+        self.adicionar_itens(ItemFritas())
+        self.adicionar_itens(ItemRefrigerante())
+
+
+class ComboFeliz(Combo):
+    def criar_combo(self):
+        self.adicionar_itens(ItemHamburger())
+        self.adicionar_itens(ItemFritas())
+        self.adicionar_itens(ItemRefrigerante())
+
+
+class ComboGelado(Combo):
+    def criar_combo(self):
+        self.adicionar_itens(ItemHamburger())
+        self.adicionar_itens(ItemSorvete())
+        
