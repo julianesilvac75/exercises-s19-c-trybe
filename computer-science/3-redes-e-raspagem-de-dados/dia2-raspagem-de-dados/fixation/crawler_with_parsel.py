0e561d1ebbafdd03a40d12ff56d8aee3ef367c09
Juliane <julianesilvac75@gmail.com>
2022-11-14 22:49:49 +0000

    fixation: get book url and its title - first example

--- computer-science/3-redes-e-raspagem-de-dados/dia2-raspagem-de-dados/fixation/crawler_with_parsel.py
+++ computer-science/3-redes-e-raspagem-de-dados/dia2-raspagem-de-dados/fixation/crawler_with_parsel.py
@@ -4,9 +4,13 @@ from parsel import Selector
 URL = "http://books.toscrape.com/"
 
 response = requests.get(URL)
-
 selector = Selector(text=response.text)
-# print(selector.css("img.thumbnail").getall())
 
-for thumbnail in selector.css("img.thumbnail").getall():
-    print(thumbnail)
+thumb_url = selector.css("div.image_container a::attr(href)").get()
+thumbnail_request = requests.get(URL + thumb_url)
+thumbnail_selector = Selector(text=thumbnail_request.text)
+book_title = thumbnail_selector.css("div.product_main h1")
+print(book_title.get())
+
+# for thumbnail in selector.css("img.thumbnail").getall():
+#     print(thumbnail)
