128fae9e1b7f42b696772d5b06c87c18edf666d1
Juliane <julianesilvac75@gmail.com>
2022-11-14 22:57:01 +0000

    fixation: dinamically getting all book titles by crawler

--- computer-science/3-redes-e-raspagem-de-dados/dia2-raspagem-de-dados/fixation/crawler_with_parsel.py
+++ computer-science/3-redes-e-raspagem-de-dados/dia2-raspagem-de-dados/fixation/crawler_with_parsel.py
@@ -5,12 +5,13 @@ URL = "http://books.toscrape.com/"
 
 response = requests.get(URL)
 selector = Selector(text=response.text)
+thumbnail_url_selector = "div.image_container a::attr(href)"
 
-thumb_url = selector.css("div.image_container a::attr(href)").get()
-thumbnail_request = requests.get(URL + thumb_url)
-thumbnail_selector = Selector(text=thumbnail_request.text)
-book_title = thumbnail_selector.css("div.product_main h1")
-print(book_title.get())
+for url in selector.css(thumbnail_url_selector).getall():
+    thumbnail_request = requests.get(URL + url)
+    thumbnail_selector = Selector(text=thumbnail_request.text)
+    book_title = thumbnail_selector.css("div.product_main h1")
+    print(book_title.get())
 
 # for thumbnail in selector.css("img.thumbnail").getall():
 #     print(thumbnail)
