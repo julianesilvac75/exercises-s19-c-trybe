a982380ef7f3dd8aba9c1e18b16e20b9141d9819
Juliane <julianesilvac75@gmail.com>
2022-11-15 18:53:52 +0000

    fixation: filter database with context (with)

--- computer-science/3-redes-e-raspagem-de-dados/dia2-raspagem-de-dados/fixation/mongodb_examples/mongo_client_example.py
+++ computer-science/3-redes-e-raspagem-de-dados/dia2-raspagem-de-dados/fixation/mongodb_examples/mongo_client_example.py
@@ -1,20 +1,26 @@
 from pymongo import MongoClient
 
 
+#Using context:
+with MongoClient() as client:
+    db = client.catalogue
+    for book in db.books.find({"title": {"$regex": "t"}}):
+        print(book["title"])
+
 #by default, host is localhost and port 27017
 #those values can be modified by passing an URI
 #client = MongoClient("mongodb://localhost:27017")
-client = MongoClient()
+# client = MongoClient()
 
 #create "catalogue" database if doesnt exist:
-db = client.catalogue
+# db = client.catalogue
 
 #search for a document in a colection (without filters):
-print(db.books.find_one())
+# print(db.books.find_one())
 
-#search with filters:
-for book in db.books.find({"title": {"$regex": "t"}}):
-    print(book["title"])
+# #search with filters:
+# for book in db.books.find({"title": {"$regex": "t"}}):
+#     print(book["title"])
 
 
 # documents = [
