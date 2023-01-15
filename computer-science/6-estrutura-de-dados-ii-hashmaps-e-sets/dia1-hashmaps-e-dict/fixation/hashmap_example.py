502ccdd9cae97bc6f542c9792632d47ff7deb9e7
Juliane <julianesilvac75@gmail.com>
2023-01-15 19:15:01 +0000

    fixation: hashmap function exercises - updating data

--- computer-science/6-estrutura-de-dados-ii-hashmaps-e-sets/dia1-hashmaps-e-dict/fixation/hashmap_example.py
+++ computer-science/6-estrutura-de-dados-ii-hashmaps-e-sets/dia1-hashmaps-e-dict/fixation/hashmap_example.py
@@ -22,3 +22,27 @@ class HashMap:
     def has(self, id_num):
         address = self.get_address(id_num)
         return self._buckets[address] is not None
+
+    def update_value(self, id_num, new_name):
+        address = self.get_address(id_num)
+        print("Old name:", self._buckets[address].name)
+        self._buckets[address].name = new_name
+        print("New name:", self._buckets[address].name)
+
+
+if __name__ == "__main__":
+    employees = [(14, "name1"), (23, "name2"), (10, "name3"), (9, "name4")]
+    hashmap = HashMap()
+
+    for id_num, name in employees:
+        employee = Employee(id_num, name)
+
+        hashmap.insert(employee)
+
+    if hashmap.has(23):
+        print(hashmap.get_value(23))
+
+    print("----------")
+
+    if hashmap.has(10):
+        hashmap.update_value(10, "name30")
