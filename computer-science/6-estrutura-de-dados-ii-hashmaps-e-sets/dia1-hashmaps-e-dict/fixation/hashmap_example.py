2b957f98d5e32e31fbd297f713ee647504868ae4
Juliane <julianesilvac75@gmail.com>
2023-01-15 19:39:24 +0000

    fixation: colision example

--- computer-science/6-estrutura-de-dados-ii-hashmaps-e-sets/dia1-hashmaps-e-dict/fixation/hashmap_example.py
+++ computer-science/6-estrutura-de-dados-ii-hashmaps-e-sets/dia1-hashmaps-e-dict/fixation/hashmap_example.py
@@ -6,28 +6,38 @@ class Employee:
 
 class HashMap:
     def __init__(self):
-        self._buckets = [None for i in range(10)]
+        self._buckets = [[] for i in range(10)]
 
     def get_address(self, id_num):
         return id_num % 10
 
     def insert(self, employee):
         address = self.get_address(employee.id_num)
-        self._buckets[address] = employee
+        self._buckets[address].append(employee)
 
     def get_value(self, id_num):
         address = self.get_address(id_num)
-        return self._buckets[address].name
+        for item in self._buckets[address]:
+            if item.id_num == id_num:
+                return item.name
+        return None
 
     def has(self, id_num):
         address = self.get_address(id_num)
-        return self._buckets[address] is not None
+        if len(self._buckets[address]) != 0:
+            for item in self._buckets[address]:
+                if item.id_num == id_num:
+                    return True
+        
+        return False
 
     def update_value(self, id_num, new_name):
         address = self.get_address(id_num)
-        print("Old name:", self._buckets[address].name)
-        self._buckets[address].name = new_name
-        print("New name:", self._buckets[address].name)
+        for item in self._buckets[address]:
+            if item.id_num == id_num:
+                print("Old name:", item.name)
+                item.name = new_name
+                print("New name:", item.name)
 
 
 if __name__ == "__main__":
